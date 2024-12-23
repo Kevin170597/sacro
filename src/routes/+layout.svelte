7fcf161cdf5b8a15e6f34c6fd80695a75036679a
Kevin Mendoza <kevin170597@gmail.com>
2024-12-23 12:41:24 -0300

    auth done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import "../app.css";
 	import { Header, Footer } from "./snippets";
+	import { page } from "$app/stores";
 
 	let { children } = $props();
 </script>
 
 <div class="min-h-[100vh] bg-slate-200">
-	<!-- <Header /> -->
+	{#if !$page.data.session?.user}
+		<Header />
+	{/if}
 	{@render children()}
-	<!-- <Footer /> -->
+	{#if !$page.data.session?.user}
+		<Footer />
+	{/if}
 </div>
--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -2,4 +2,22 @@
     import { SignIn } from "@auth/sveltekit/components";
 </script>
 
-<SignIn provider="google" />
+<div class="h-[92vh] bg-slate-100 py-4">
+    <h1 class="text-[24px] font-bold text-center mb-4">Iniciar sesión</h1>
+    <div
+        class="w-2/5 mx-auto px-8 py-4 bg-white border border-slate-300 rounded-lg"
+    >
+        <h2 class="mb-4 font-bold text-[24px]">
+            <span class="text-sky-500">G</span>
+            <span class="text-red-500">o</span>
+            <span class="text-yellow-500">o</span>
+            <span class="text-sky-500">g</span>
+            <span class="text-green-500">l</span>
+            <span class="text-red-500">e</span>
+        </h2>
+        <SignIn
+            provider="google"
+            className="text-sky-600 font-bold border bg-sky-100 border-slate-300 py-4 px-8 rounded-lg text-center"
+        />
+    </div>
+</div>
