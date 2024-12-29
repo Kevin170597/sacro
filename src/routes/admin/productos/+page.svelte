48bc01c2f4dc4246f95ded8ff55fb64f0e929f43
Kevin Mendoza <kevin170597@gmail.com>
2024-12-29 14:03:22 -0300

    admin product card size changed

--- src/routes/admin/productos/+page.svelte
+++ src/routes/admin/productos/+page.svelte
@@ -8,9 +8,9 @@
 <svelte:head><title>Admin - Productos</title></svelte:head>
 <div class="h-[92vh] overflow-auto px-4 sm:px-8 py-4 bg-slate-100 flex flex-col gap-4">
     <div class="flex items-center">
-        <h1 class="text-[20px] font-bold sm:text-[16px]">Productos</h1>
+        <h1 class="text-[14px] font-bold">Productos</h1>
         <a
-            class="bg-sky-500 hover:bg-sky-600 text-white w-fit ml-auto px-4 py-2 rounded-lg"
+            class="bg-sky-500 hover:bg-sky-600 text-white w-fit ml-auto px-3 py-1 rounded-lg"
             href="/admin/productos/nuevo"
         >
             <span class="font-bold text-[12px]">Publicar</span>
--- src/routes/admin/productos/snippets/ProductCard.snippet.svelte
+++ src/routes/admin/productos/snippets/ProductCard.snippet.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
     import type { Product } from "$lib/interfaces";
+    import { Button } from "$lib/components";
     import { enhance } from "$app/forms";
 
     let { product }: { product: Product } = $props();
 </script>
 
-<div class="flex bg-white rounded-lg border border-slate-300">
+<div class="flex bg-white p-2 rounded-lg border border-slate-300">
     <div class="w-[6%] flex justify-center items-center">
         <img class="p-2" src={product.variants[0].images[0]} alt="" />
     </div>
     <div class="py-2 px-4 w-[44%]">
-        <p class="text-[14px] text-slate-400">{product._id}</p>
+        <p class="text-[11px] text-slate-400"># {product._id}</p>
         <a class="hover:underline" href="/admin/productos/{product._id}">
-            <span class="font-bold text-[14px]">{product.title}</span>
+            <span class="font-bold text-[12px]">{product.title}</span>
         </a>
     </div>
-    <div class="py-2 px-4 flex flex-wrap items-center w-[20%] gap-2">
+    <div class="py-2 px-4 grid grid-cols-6 items-start w-[20%] gap-2">
         {#each product.variants as variant}
             <div
-                class="w-5 h-5 rounded-full border border-slate-400"
+                class="w-4 h-4 rounded-full border border-slate-400"
                 style:background-color={variant.hexColor}
             ></div>
         {/each}
@@ -33,11 +34,12 @@
         use:enhance
     >
         <input type="text" value={product._id} hidden name="id" />
-        <button
+        <Button
+            label="Eliminar"
+            color="rose"
+            rounded="lg"
+            size="md"
             type="submit"
-            class="px-4 py-2 bg-red-100 hover:bg-red-200 transition-all duration-200 rounded-lg"
-        >
-            <span class="font-bold text-[12px] text-red-600">Eliminar</span>
-        </button>
+        />
     </form>
 </div>
